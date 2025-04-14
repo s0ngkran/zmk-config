40a70c6dca0aa110e12aa1a9801845e0cc1419ef
s0ngkran <jinnawat8@gmail.com>
2025-04-14 18:33:33 +0700

    pointing work

--- build_sk_keyboard.sh
+++ build_sk_keyboard.sh
@@ -1,12 +1,12 @@
 
 echo "Building SK keyboard..."
 
-docker run -it --rm -v /Users/walter/projects/zmk-config:/workspace zmk bash -c "cd /workspace && west build -s zmk/app -b nice_nano_v2 -- -DSHIELD=cp44 -DZMK_CONFIG=/workspace/config -DCMAKE_POLICY_VERSION_MINIMUM=3.5";
+docker run -it --rm -v /Users/walter/projects/zmk-config:/workspace updated_zmk bash -c "cd /workspace && west build -s zmk/app -b nice_nano_v2 -- -DSHIELD=cp44 -DZMK_CONFIG=/workspace/config -DCMAKE_POLICY_VERSION_MINIMUM=3.5";
 
-echo "Opening build directory..."
-echo
-echo "build/zephyr/zmk.uf2"
-echo
+# echo "Opening build directory..."
+# echo
+# echo "build/zephyr/zmk.uf2"
+# echo
 
 # open build/zephyr/
 
